6993ed583d5be77ff7eb3e53c751d7c155d66fac
jeffvippan <www.jeffpan@gmail.com>
2018-04-16 15:11:53 +0800

    init commit

--- src/main/java/com/eis/upgrade/UpgradeApplication.java
+++ src/main/java/com/eis/upgrade/UpgradeApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
@@ -12,6 +13,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableDiscoveryClient
 @EnableTransactionManagement
 @MapperScan("com.eis.*.dao")
+@ComponentScan("com.eis.*.service")
 public class UpgradeApplication {
 
     public static void main(String[] args) {
